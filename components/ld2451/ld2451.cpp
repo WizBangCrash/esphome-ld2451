fa81470a4a67763521dbdb17bbcffd8eae98349a
David Dix <david@dixieworld.co.uk>
2026-09-14 16:17:41 +0100

    Bump component version 2.0.1dev

--- components/ld2451/ld2451.cpp
+++ components/ld2451/ld2451.cpp
@@ -32,7 +32,7 @@
 namespace esphome::ld2451 {
 
 static const char *const TAG = "ld2451";
-static const char *const COMPONENT_VERSION = "2.0.0";
+static const char *const COMPONENT_VERSION = "2.0.1dev";
 
 static constexpr uint8_t CMD_HEADER[4] = {0xFD, 0xFC, 0xFB, 0xFA};
 static constexpr uint8_t CMD_FOOTER[4] = {0x04, 0x03, 0x02, 0x01};
